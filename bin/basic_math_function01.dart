bb557eb23ec77a58ef051b6ec87bdc3ca9127504
ahror97 <abdullayev.ahror97@gmail.com>
2023-05-12 14:55:06 +0500

    first commit

---
+++ bin/basic_math_function01.dart
@@ -0,0 +1,17 @@
+/*
+  Create function named func with a argument
+  Given the squre area,find its side
+  Args:
+      square_area (int): the area of the square
+  Returns:test
+      side (double): the side of the square
+*/
+import 'dart:math';
+
+double func(int s) {
+  return sqrt(s);
+}
+
+void main() {
+  print(func(25));
+}
---
+++ bin/basic_math_function02.dart
@@ -0,0 +1,17 @@
+/*
+  Create function named func with a argument
+  Given the radius of a circle, find its length (L=2Rπ)
+  Args:
+      R (int): the radius of the circle
+  Returns:
+      L (double): the length of the circle
+*/
+import 'dart:math';
+
+double func(int r) {
+  return 2 * pi * r;
+}
+
+void main() {
+  print(func(3));
+}
